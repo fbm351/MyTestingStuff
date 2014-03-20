e60ec49d4c022a12e8290378743af26b8e41e353
Fred Myers <fred@fred-myers.com>
2014-03-20 15:52:45 -0400

    Added Profile VC

---
+++ MyTestingStuff/FMProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMProfileTableViewController.h
+//  MyTestingStuff
+//
+//  Created by Fredrick Myers on 3/20/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMProfileTableViewController : UITableViewController
+
+@end
